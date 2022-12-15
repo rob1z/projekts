59b7063ff93d563f59f13df19c2fb3b8bf697389
Wolfy122!! <roberts.savickis@valmierastehnikums.lv>
2022-12-15 15:10:12 +0200

    csgopage

--- resources/views/videos/cssmokes.blade.php
+++ resources/views/videos/cssmokes.blade.php
@@ -4,113 +4,101 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
     <title>Document</title>
 </head>
-<body>
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Video Gallery</title>
-</head>
 <style>
-    *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-}
-
-body{
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    background: cornflowerblue;
-}
-
-.container{
-    width: 1100px;
-    height: 480px;
-    display: flex;   
-    background: rgb(238, 236, 236);
-}
-
-.container .iframes{
-    width: 20%;
-    padding: 10px 0 10px 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-}
-
-.container .iframes iframe{
-    width: 95%;
-    height: 100px;
-    margin: 10px;
-    object-fit: cover;
-    cursor: pointer;
-    transition: 0.2s;
-}
-
-.container .iframes iframe:nth-child(1){
-    margin-top: 0;
-}
-
-.container .iframes iframe:hover,
-.container .iframes .active{
-    transform: scale(1.06);
-    border: 3px solid blue;
-}
-
-.container .main-iframe{
-    width: 80%;
-    padding: 10px;
-}
-
-.container .main-iframe iframe{
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border: 3px solid blue;
-}
+    .collapsible {
+        max-width:450px;
+        overflow: hidden;
+        font-weight: 500;
+    }
+    .collapsible input{
+        display: none;
+    }
+    .collapsible label{
+        position: relative;
+        font-weight:600;
+        background: #fff;
+        box-shadow: 0 5px 11px 0 rgba(0, 0, 0, .1), 0 4px 11px 0 rgba(0, 0, 0, .08);
+        color: #1c1c6b;
+        display:block;
+        margin-bottom: 10px;
+        cursor: pointer;
+        padding: 15px;
+        border-radius: 4px;
+        z-index: 1;
+    }
+    .collapsible label:after{
+        content: "";
+        position: absolute;
+        right: 15px;
+        top: 15px;
+        width: 18px;
+        height: 18px;
+        background: url(arrow.svg) no-repeat 0 0;
+        transition: all 0.3s ease;
+    }
+    .collapsible input:checked + label:after{
+        transform: rotate(90deg);
+    }
+    .collapsible-text{
+        max-height:1px;
+        overflow:hidden;
+        border-radius: 4px;
+        line-height: 1.4;
+        position:relative;
+        top:-100%;
+        opacity: 0.5;
+        transition: all 0.3s ease;
+    }
+    .collapsible input:checked ~ .collapsible-text{
+        max-height: 300px;
+        padding-bottom: 25px;
+        background: #fff;
+        box-shadow: 0 5px 11px 0 rgba(0, 0, 0, .1), 0 4px 11px 0 rgba(0, 0, 0, .08);
+        opacity: 1;
+        top: 0;
+    }
+    .collapsible-text h2 {
+        margin-bottom: 10px;
+        padding: 15px 15px 0;
+        color: #1c1c6b;
+    }
+    .collapsible-text p{
+        padding-left:15px;
+        padding-right:15px;
+    }
+    .content{
+        display:flex;
+        justify-content:center;
+    }
 </style>
 <body>
-
-    <div class="container">
-        <div class="iframes">
-            <video class="active" src="https://www.youtube.com/watch?v=SgXsZNNponM" muted></video>
-            <iframe src="https://www.youtube.com/embed/_mza5clVD-k"></iframe>
-            <video src="https://www.youtube.com/watch?v=psUwkbpYbV0" muted></video>
-            <video src="https://www.youtube.com/watch?v=SgXsZNNponM" muted></video>
-        </div>
-        <div class="main-video">
-            <video src="https://www.youtube.com/watch?v=_mza5clVD-k" muted controls autoplay></video>
+    <div class="content">
+    <div class="wrapper">
+        <div class="collapsible">
+            <input type="checkbox" id="collapsible-head">
+            <label for="collapsible-head"> Anubis Smoke Tutorial </label>
+            
+            <div class="collapsible-text">
+                <h2> Smoke Lineups </h2>
+                    <iframe width="400" height="300" src="https://www.youtube.com/embed/_mza5clVD-k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
-
-
-    <script>
-
-        $(document).ready(function(){
-
-            $('.iframes iframe').click(function(){
-
-                $(this).addClass('active').siblings().removeClass('active');
-
-                var src = $(this).attr('src');
-                $('.main-iframe iframe').attr('src',src);
-            });
-        });
-
-    </script>
-
-</body>
-</html>
+</div>
+</div>
+    <div class="content">
+        <div class="wrapper">
+<div class="collapsible">
+    <input type="checkbox" id="collapsible-head">
+    <label for="collapsible-head"> Inferno Smoke Tutorial </label>
     
+    <div class="collapsible-text">
+        <h2> Smoke Lineups </h2>
+            <iframe width="400" height="300" src="https://www.youtube.com/embed/gjekrf9NvGM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</div>
+</div>
 </body>
 </html>
